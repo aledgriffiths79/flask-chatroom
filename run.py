f251cb12787585f58ef8572c441ab75c80664d8d
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-06-15 17:40:50 +0100

    adding onto the index view a session variable

--- run.py
+++ run.py
@@ -2,10 +2,11 @@
 import os
 # import datetime module from the datetime library which is a built in module in pythons standard library that allows us to work specifically with dates and times
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
 
 # initialise our new flask application
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 def add_messages(username, message):
@@ -18,10 +19,18 @@ def get_all_messages():
   return "<br>".join(messages)
 
 # create our app root decorator, which is going to be for our index page so that will be ("/")
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 # define the function that is going to be bound to our decorator: def.index().
 def index():
   """Main page with instructions"""
+
+  if request.method == "POST":
+    # create session username variable
+    session["username"] = request.form["username"]
+
+  if "username" in session:
+    return redirect(session["username"])
+
   return render_template("index.html")
 
 @app.route("/<username>")
