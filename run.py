1a75973f6cd07f5e227c7755e89c3aa3e1bf0832
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-06-15 12:15:32 +0100

    appending strings

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
 # import os so that we will have access to the environment variables
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 # initialise our new flask application
 app = Flask(__name__)
 messages = []
 
 def add_messages(username, message):
-  message.append("{}: {}".format(username, message))
+  """Add messages to the 'messages' list"""
+  messages.append("{}: {}".format(username, message))
+
+def get_all_messages():
+  """Get all the messages and separate them with a 'br'"""
+  return "<br>".join(messages)
 
 # create our app root decorator, which is going to be for our index page so that will be ("/")
 @app.route("/")
@@ -19,13 +24,14 @@ def index():
 @app.route("/<username>")
 def user(username):
   """Display chat messages"""
-  return "Welcome, {0}".format(username, messages)
+  return "Welcome, {0} - {1}".format(username, get_all_messages())
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
   # Docstrings for my function (notes to understand what going on using """write here""")
   """ Create a new message and redirect back to the chat page """
-  return "{0}: {1}".format(username, message)
+  add_messages(username, message)
+  return redirect("/" + username)
 
 app.run(debug=True)
 # app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')))
